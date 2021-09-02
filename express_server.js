362a3aadc3f3a7f593a58be16146f46c1eb26a8c
Zubaer <bijon7@yahoo.com>
2021-09-01 23:27:20 -0700

    add log in and log out featureto the homepage

--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 function generateRandomString() {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
-  for ( var i = 0; i < 5; i++ ) {
+  for (var i = 0; i < 5; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
@@ -33,7 +34,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {  urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"], };
   res.render("urls_index", templateVars);
 });
 
@@ -42,7 +43,8 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"], };
+
   res.render("urls_show", templateVars);
 });
 
@@ -56,7 +58,7 @@ app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   //Add the new LongURL with the ShortURL in the URlDatabase
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect("/urls/"+shortURL); //"urls/23tx"
+  res.redirect("/urls/" + shortURL); //"urls/23tx"
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
@@ -67,6 +69,23 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  console.log("here");
+  const username = req.body.username;
+  console.log(username);
+
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  console.log(res);
+  
+  res.clearCookie("username");
+  // res.clearCookie("username", username, {expire: Date.now()});
+  res.redirect("/urls");
+
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
